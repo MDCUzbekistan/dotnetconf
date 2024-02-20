3993b31e3dca4f192693d5f4e33443715281e61b
MarufZak <marufzak04@gmail.com>
2024-02-20 13:01:07 +0500

    ✨ feat: error page

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+import Heading from "@/components/heading/heading";
+import React from "react";
+
+const Error = () => {
+  return (
+    <section className="not-found">
+      <Heading className="not-found-title" level={3}>
+        Something went wrong, please try again later...
+      </Heading>
+    </section>
+  );
+};
+
+export default Error;
