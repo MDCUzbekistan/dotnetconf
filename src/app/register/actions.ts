b90ed8356a7f8917bc98f620889017a596b8ac61
MarufZak <marufzak04@gmail.com>
2024-02-20 13:47:45 +0500

    🐛 fix: bug in junior position in registration

--- src/app/register/actions.ts
+++ src/app/register/actions.ts
@@ -10,7 +10,7 @@ const RegistrationSchema = z.object({
   preferredLanguage: z.coerce.number(),
   country: z.string().min(1, "Please provide a country"),
   city: z.string().min(1, "Please provide a city"),
-  role: z.coerce.number().min(1, "Please provide a role"),
+  role: z.coerce.number(),
   position: z.coerce.number().min(1, "Please provide a position"),
   expectation: z.string().min(1, "Please provide an expectation"),
   teamname: z.string().min(1, "Please provide a team name"),
